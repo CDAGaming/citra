d6adbb7172159220e0bd62a9357cc3ed7db8fb76
CDAGaming <cstack2011@yahoo.com>
2017-11-24 18:28:48 -0600

    Update Linux Commands and fix PATH Errors in MacOS (Travis)

--- .travis/linux/deps.sh
+++ .travis/linux/deps.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -ex
 
 docker pull ubuntu:16.04
+sudo apt-get update
+sudo apt-get upgrade
--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-rvm get head
+rvm get head --auto-dotfiles
 brew update
 brew upgrade
 brew install qt5 sdl2 dylibbundler p7zip
