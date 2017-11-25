ee33c0be0fdb2a9bbf4fe3c0858276c6af9bfdd5
CDAGaming <cstack2011@yahoo.com>
2017-11-24 19:12:09 -0600

    Missing V9.5

--- .travis/linux/deps.sh
+++ .travis/linux/deps.sh
@@ -5,6 +5,6 @@ sudo apt-get remove -y postgresql-9.1
 sudo apt-get remove -y postgresql-9.2
 sudo apt-get remove -y postgresql-9.3
 sudo apt-get remove -y postgresql-9.4
-sudo apt-get remove -y postgresql-9.4
+sudo apt-get remove -y postgresql-9.5
 sudo apt-get update -q
 sudo apt-get upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
